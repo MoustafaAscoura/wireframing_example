58b25768e34931732751c44493624194f6beda7a
Moustafa Ascoura <askoura2008@yahoo.com>
2018-03-03 17:41:59 +0200

    Class Topic and Member

---
+++ Forum/main.py
@@ -0,0 +1,6 @@
+import models
+member1= models.Member("Moustafa",19)
+member2=models.Member("Ahmed",25)
+post1=models.Topic("Introduction","Welcome to our forum")
+post2=models.Topic("What Can you do ? ","You can change your name and age")
+post3=models.Topic("Help others","You can help others by answering questions")
